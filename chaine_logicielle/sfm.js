b6c5b0674ac2a616fef3d3b27e6bc514bbc745e7
guroy <guroy@insa-rennes.fr>
2014-02-17 22:01:01 +0100

    appel via tableau de commandes socket VisualSFM

--- chaine_logicielle/sfm.js
+++ chaine_logicielle/sfm.js
@@ -1,5 +1,11 @@
 var spawn = require('child_process').spawn,
-    ls    = spawn('VisualSFM', ['sfm+pmvs',  'data/lion', 'data/res.nvm']);
+	net = require('net'),
+    ls    = spawn('VisualSFM', ['listen', '9999']);
+var tabCommands = 	['33166 data\\list.txt\n', //Open+ Multi Images
+					 '33033\n', //Compute Missing Match
+					 '33041\n', //Reconstruct Sparse
+					 '33471 E:\\Mes documents\\GitHub\\cnpao\\chaine_logicielle\\data\\output.nvm\n']; //Reconstruct Dense
+var ident = 0;
 
 ls.stdout.on('data', function (data) {
   console.log('stdout: ' + data);
@@ -17,4 +23,31 @@ ls.on('close', function (code) {
   console.log('child process exited with code ' + code);
 });
 
+var client = net.connect({port: 9999}, function() {
+	console.log('client connected');
+	callNextCommand();
+});
 
+function callNextCommand(){
+	if(ident < tabCommands.length) client.write(tabCommands[ident++]);
+}
+
+/**
+/* function charge la commande suivante quand elle lit '*command processed*'
+/* @param data, la ligne de log
+*/
+client.on('data', function(data) {
+  console.log(data.toString());
+  	/* indexOf compare une chaine avec la chaine en parametre
+	   retourne -1 si rien trouve, ou bien la position de la chaine trouvee */
+	if(data.toString().indexOf('*command processed*')>-1) {
+		callNextCommand(); //Compute Missing Match
+	}
+});
+
+client.on('error', function(err) {
+  console.log('error '+err);
+});
+client.on('end', function() {
+  console.log('client disconnected');
+});
