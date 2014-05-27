3f2304ccc38a8c72026f8ef01a80914cf870048d
hyukchan <hyukchan@MacBook-Air-de-Hyukchan.local>
2014-05-27 17:15:18 +0200

    nouislider

--- site_web/header.php
+++ site_web/header.php
@@ -7,4 +7,7 @@
     <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="FileUpload/css/jquery.fileupload.css" type="text/css">
     <link rel="stylesheet" href="FileUpload/css/jquery.fileupload-ui.css" type="text/css">
+    <link rel="stylesheet" href="nouislider/jquery.nouislider.css" type="text/css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js"></script>
+    <script src="nouislider/jquery.nouislider.min.js"></script>
 </head>
